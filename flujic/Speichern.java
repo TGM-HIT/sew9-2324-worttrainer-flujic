a16810192b95bef56954f891a80245b21e4be029
flujic <flujic@student.tgm.ac.at>
2024-01-23 14:04:19 +0100

    Definieren der Schnittstelle, die Methoden für das Laden und Speichern der Trainingsdaten spezifiziert.

--- flujic/Speichern.java
+++ flujic/Speichern.java
@@ -1,4 +1,9 @@
 package flujic;
 
-public class Speichern  {
+import java.io.IOException;
+public interface Speichern {
+
+    void saveContent(String filePath, WortTrainer wordTrainer) throws IOException;
+    WortTrainer loadContent(String filePath, WortTrainer wordTrainer);
 }
+
