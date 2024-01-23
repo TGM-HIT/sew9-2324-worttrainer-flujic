9fad56c60c135767b35938e0cc6d3bc11b9f7763
flujic <flujic@student.tgm.ac.at>
2024-01-23 19:08:13 +0100

    Abstrakte Methode saveContent automatisch implementiert (von Speichern Interface)

---
+++ src/main/java/flujic/JSONSave.java
@@ -0,0 +1,17 @@
+package flujic;
+
+import java.io.IOException;
+
+public class JSONSave implements Speichern{
+
+
+    @Override
+    public void saveContent(String filePath, WortTrainer wordTrainer) throws IOException {
+
+    }
+
+    @Override
+    public WortTrainer loadContent(String filePath, WortTrainer wordTrainer) {
+        return null;
+    }
+}
